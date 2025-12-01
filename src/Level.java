f3e12c29dc069ea06057a4bb7580216c9c7afe30
jmiao-eng <jmiao@macalester.edu>
2025-12-01 09:42:09 -0600

    Level and Square

--- src/Level.java
+++ src/Level.java
@@ -1,3 +1,11 @@
 public class Level {
-    
+    private String backgroundPath;
+
+    public Level(String backgroundPath) {
+        this.backgroundPath = backgroundPath;
+    }
+
+    public String getBackgroundPath() {
+        return backgroundPath;
+    }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,11 +1,43 @@
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Rectangle;
+import java.awt.Color;
 
 
 public class Square {
+    public static final double SIZE = 30;
     private Rectangle rect;
-    private int lane;
-    private double yPosition;
+    private int lane; 
+    private double speed;
 
 
+     public Square(int lane, double x, double y, double speed, CanvasWindow canvas) {
+        this.lane = lane;
+        this.speed = speed;
+
+        rect = new Rectangle(x, y, SIZE, SIZE);
+        rect.setFillColor(Color.WHITE);
+        rect.setFilled(true);
+        rect.setStroked(false);
+        canvas.add(rect);
+    }
+
+     public void update() {
+        rect.setY(rect.getY() + speed);
+    }
+
+    public double getY() {
+        return rect.getY();
+    }
+
+    public int getLane() {
+        return lane;
+    }
+
+    public Rectangle getShape() {
+        return rect;
+    }
+
+    public void removeFromCanvas(CanvasWindow canvas) {
+        canvas.remove(rect);
+    }
 }
